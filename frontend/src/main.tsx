ce0d8ed280635e299a27a949caa8ed48e50cefce
Luciano Villanueva <lucianoivillanuevar@gmail.com>
2026-08-12 23:25:16 -0300

    chore(deps): actualizar dependencias auditadas

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,7 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true
-      }}
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
